0279dde2ea3e13b37574ddc97f370bbfed8f2069
Your Name <you@example.com>
2023-10-07 18:04:15 -0500

    Navbar

--- src/context/data/myState.jsx
+++ src/context/data/myState.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MyContext from './myContext'
 
 function myState(props) {
-    const state = {
-        name: "Brayan Ospina",
-        rollno: 15,
+  const [mode, setMode] = useState('light');
+
+  const toggleMode = () => {
+    if(mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = "rgb(17,24,30)"
     }
+    else{
+      setMode('ligth');
+      document.body.style.backgroundColor = "white"
+    }
+  }
+    
   return (
-    <MyContext.Provider value={state}>
+    <MyContext.Provider value={{mode, toggleMode}}>
         {props.children}
     </MyContext.Provider>
   )
