ce3d3c52082641365467f95f6afc5d465ac5ce8b
brayanospina2005 <ospinabrayan431@gmail.com>
2023-10-25 17:34:20 -0500

    signup with firebase

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,8 @@ import Signup from "./pages/registration/Signup";
 import ProductInfo from "./pages/productInfo/ProductInfo";
 import AddProduct from "./pages/admin/page/AddProduct";
 import UpdateProduct from "./pages/admin/page/UpdateProduct";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
@@ -28,6 +30,7 @@ function App() {
           <Route path="/updateproduct" element={< UpdateProduct />} />
           <Route path="/*" element={<NoPage />} />
         </Routes>
+        <ToastContainer />
       </Router>
     </MyState>
   );
