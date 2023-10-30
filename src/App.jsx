2c7011719cd1dd3086dcfe915f8b0d64161471e3
Your Name <you@example.com>
2023-10-29 21:19:47 -0500

    login

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Order from "./pages/order/Order";
 import Cart from "./pages/cart/Cart";
@@ -20,14 +20,30 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/order" element={<Order />} />
+          <Route path="/order" element={
+            <ProtectedRoute>
+              <Order />
+            </ProtectedRoute>
+          } />
           <Route path="/cart" element={<Cart />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/dashboard" element={
+            <ProtectedRouteForAdmin>
+              <Dashboard />
+            </ProtectedRouteForAdmin>
+          } />
           <Route path="/login" element={< Login />} />
           <Route path="/signup" element={< Signup />} />
           <Route path="/productinfo/:id" element={< ProductInfo />} />
-          <Route path="/addproduct" element={< AddProduct />} />
-          <Route path="/updateproduct" element={< UpdateProduct />} />
+          <Route path="/addproduct" element={
+            <ProtectedRouteForAdmin>
+              <AddProduct/>
+            </ProtectedRouteForAdmin>
+          } />
+          <Route path="/updateproduct" element={
+            <ProtectedRouteForAdmin>
+              <UpdateProduct/>
+            </ProtectedRouteForAdmin>
+          } />
           <Route path="/*" element={<NoPage />} />
         </Routes>
         <ToastContainer />
@@ -37,3 +53,28 @@ function App() {
 }
 
 export default App;
+
+// user 
+
+export const ProtectedRoute = ({children}) => {
+  const user = localStorage.getItem('user')
+  if(user) {
+    return children
+  }
+  else{
+    return <Navigate to={'/login'} />
+  }
+}
+
+// admin 
+
+const ProtectedRouteForAdmin = ({children}) => {
+  const admin = JSON.parse(localStorage.getItem('user'))
+
+  if(admin.user.email === 'prueba@gmail.com'){
+    return children
+  }
+  else{
+    return <Navigate to={'/login'} />
+  }
+}
